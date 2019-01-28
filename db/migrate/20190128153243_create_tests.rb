aa1dad048a04e3c17e5394589d525698f5affee3
Roman. Z <zworkbox@yandex.ru>
2019-01-28 18:33:58 +0300

    Добавлена модель Test

---
+++ db/migrate/20190128153243_create_tests.rb
@@ -0,0 +1,12 @@
+class CreateTests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tests do |t|
+      t.string :title
+      t.integer :level
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :tests, :title, unique: true
+  end
+end
