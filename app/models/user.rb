c6d6101b942d2e1fb464dd58412bea5d26e9381c
Roman. Z <zworkbox@yandex.ru>
2019-02-02 20:28:48 +0300

    + Добавлен метод отбора тестов пользователя по заданной сложности

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  def test_passages(test_level)
+    Test.joins("
+      INNER JOIN user_tests AS passage
+        ON tests.id = passage.test_id
+      INNER JOIN users
+        ON users.id = passage.user_id
+    ").
+    where(
+      tests: { level: test_level },
+      users: { id: self.id }
+    )
+  end
+
 end
