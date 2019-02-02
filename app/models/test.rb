2f0f58ca24c67f21799a63d8f32dee441d64e575
Roman. Z <zworkbox@yandex.ru>
2019-02-02 23:32:12 +0300

    + Добавлен отбор названий тестов по названию родительской категории.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.pluck_by_category(title)
+    self.joins("
+      INNER JOIN categories ON tests.category_id = categories.id
+    ").
+    where(
+      categories: { title: title }
+    ).
+    pluck(:title)
+  end
+
 end
