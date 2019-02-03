7eefac538761533b54e1806b206e74dcd1f46bc5
Roman. Z <zworkbox@yandex.ru>
2019-02-03 06:16:09 +0300

    + Добавлена сортировка в отбор названий тестов.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,15 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.pluck_by_category(title)
+  def self.take_titles_by_category(title)
     self.joins("
-      INNER JOIN categories ON tests.category_id = categories.id
+      INNER JOIN categories
+        ON tests.category_id = categories.id
     ").
     where(
       categories: { title: title }
     ).
+    order(title: :desc).
     pluck(:title)
   end
 
