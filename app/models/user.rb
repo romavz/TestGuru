9ca569b96f3f469943a156c2c48e5e961c1ec175
Roman. Z <zworkbox@yandex.ru>
2019-02-06 20:07:43 +0300

    Для всех моделей, имеющих ссылки владения has_many, задано поведение при удалении в отношении подчиненных сущностей. Для ассоциаций belongs_to, заданы внешние ключи.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  has_many :user_tests
-  has_many :tests, through :user_tests
-  has_many :owned_tests, class_name: "Test"
+  has_many :user_tests, dependent: :destroy
+  has_many :tests, {
+    through: :user_tests,
+    dependent: :destroy
+  }
+
+  has_many :owned_tests, {
+    class_name: "Test",
+    inverse_of: :author,
+    dependent: :restrict_with_exception
+  }
 
   def test_passages(test_level)
     Test.joins("
