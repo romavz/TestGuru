b2e1ffd981ee1f6999e29c7d6eac40f6f78c3192
Roman. Z <zworkbox@yandex.ru>
2019-02-10 00:38:12 +0300

    Добавлено поле email в модель User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_002042) do
+ActiveRecord::Schema.define(version: 2019_02_09_202323) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_02_05_002042) do
     t.string "nick_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
     t.index ["nick_name"], name: "index_users_on_nick_name", unique: true
   end
 
