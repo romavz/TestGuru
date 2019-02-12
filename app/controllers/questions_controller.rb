0709907219e3c052d85844da21e9fb0e558f4b93
Roman. Z <zworkbox@yandex.ru>
2019-02-12 12:21:49 +0300

    Исправление замечаний #1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,12 @@
 class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
-  before_action :fill_test_info, only: %i[index new create]
-  before_action :fill_question_info, only: %i[show edit update destroy]
+  before_action :load_test, only: %i[index new create]
+  before_action :load_question, only: %i[show edit update destroy]
 
-  def index
-    @test_title = @test.title
-    @questions = @test.questions
-
-  end
-
-  def show
-    @test_title = @question.test.title
-    @answers = @question.answers
-    @question_text = @question.body
-  end
-
-  def new
-
-  end
+  def index; end
+  def show; end
+  def new; end
 
   def create
     question_body = params[:question][:body]
@@ -26,36 +14,24 @@ class QuestionsController < ApplicationController
 
     Question.create( test: @test, body: question_body )
     redirect_to test_questions_path
-
   end
 
-  def edit
-
-  end
-
-  def update
-
-  end
+  def edit; end
+  def update; end
 
   def destroy
-    test_id = @question.test_id
-    return_url = test_questions_path(test_id)
-
     @question.destroy
-
-    redirect_to return_url
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
 
-  def fill_test_info
-    test_id = params[:test_id].to_i
-    @test = Test.find(test_id)
+  def load_test
+    @test = Test.find(params[:test_id])
   end
 
-  def fill_question_info
-    question_id = params[:id].to_i
-    @question = Question.find(question_id)
+  def load_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_record_not_found
