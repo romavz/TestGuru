a5dcd3732f2d91aefc599916161cb92ecee38d40
Roman <zworkbox@yandex.ru>
2019-02-15 19:55:55 +0300

    В представление tests/index добавлена колонка с количеством вопросов по каждому тесту

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,7 @@ class TestsController < ApplicationController
   before_action :load_test, only: %i[show edit update destroy]
 
   def index
-    @tests = Test.all
+    @tests = Test.includes(:category, :author, :questions)
   end
 
   def show; end
