10cd44bd435f7faa2697a95be64687812127d46b
Roman <zworkbox@yandex.ru>
2019-02-17 09:10:57 +0300

    Исправлен вывод ошибок редактрирования ответов в форме answers/_form

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -49,6 +49,6 @@ class AnswersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def answer_params
-      params.fetch(:answer, {})
+      params.require(:answer).permit(:body, :correct)
     end
 end
