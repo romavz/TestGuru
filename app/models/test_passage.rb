6eb1fbfa98eeddd785927a54da1634d4662eb0ab
Roman <zworkbox@yandex.ru>
2019-02-19 00:51:07 +0300

    Установка следующего вопроса в модели TestPassage с помощью обратного вызова

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,28 +4,35 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: "Question", optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_save :before_save_set_next_question, on: :update
+
+  def completed?
+    current_question.nil?
+  end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    save
+    save!
   end
 
 
   private
 
-    def before_validation_set_first_question
-      self.current_question = test.questions.first if test.present?
-    end
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
 
-    def correct_answer?(answer_ids)
-      correct_answers.ids.sort == answer_ids.map(&:to_i).sort
-    end
+  def before_save_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 
-    def correct_answers
-      current_question.answers.correct
-    end
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
 
 end
