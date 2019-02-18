e1fdbc670ab0e7c6e9630626e29a608ce5258f02
Roman <zworkbox@yandex.ru>
2019-02-18 18:06:05 +0300

    Замена модели UserTest на TestPassage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :user_tests, dependent: :destroy
-  has_many :tests, through: :user_tests, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
   has_many :owned_tests, class_name: "Test", inverse_of: :author, foreign_key: "author_id", dependent: :restrict_with_exception
 
   validates :email, presence: true
 
-  def test_passages(test_level)
-    tests.where(level: test_level)
-  end
+  # def test_passages(test_level)
+  #   tests.where(level: test_level)
+  # end
 
 end
