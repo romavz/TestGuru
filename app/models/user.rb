1d38c0ee5e0d4f0ebeffdec344f46c84441a6097
Roman <zworkbox@yandex.ru>
2019-02-25 17:35:06 +0300

    установка Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   EMAIL_REGEXP = /@/.freeze
 
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :trackable,
+          :validatable,
+          :confirmable
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many  :owned_tests, class_name: "Test", inverse_of: :author, foreign_key: "author_id",
             dependent: :restrict_with_exception # rubocop:disable Layout/AlignHash
-
-  has_secure_password
 
   validates :email,
             presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root to: "tests#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get :signup, to: "users#new"
-  get :login, to: "sessions#new"
-  delete :logout, to: "sessions#destroy"
-
-  resources :users, only: :create
-  resources :sessions, only: :create
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
