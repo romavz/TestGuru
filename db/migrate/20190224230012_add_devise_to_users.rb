9d1116c804bd9156615135582ecaf07f5eb83b44
Roman <zworkbox@yandex.ru>
2019-02-25 21:21:40 +0300

    Включена аутентификация Devise. Добавлены представления Devise.

--- db/migrate/20190224230012_add_devise_to_users.rb
+++ db/migrate/20190224230012_add_devise_to_users.rb
@@ -14,11 +14,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
@@ -30,7 +30,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
