0119e3b005aca5d9667f0b4f39c4712e035ca505
Roman <zworkbox@yandex.ru>
2019-02-27 17:42:46 +0300

    Добавлено пространство администратора Представления вопросов и ответов перенесены в раздел admin

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,38 +1,8 @@
 class TestsController < ApplicationController
-  before_action :load_test, only: %i[start show edit update destroy]
+  before_action :load_test, only: %i[start]
 
   def index
     @tests = Test.includes(:category, :author, :questions)
-  end
-
-  def show; end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-    if @test.save
-      redirect_to tests_path
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -46,7 +16,4 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-  def test_params
-    params.require(:test).permit(:title, :level, :category, :author)
-  end
 end
