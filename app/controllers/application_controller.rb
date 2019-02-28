a9b69f96f662cb8eb9122cabdf20a0526a53c0c1
Roman <zworkbox@yandex.ru>
2019-02-28 11:05:56 +0300

    Добавлено разрешение на дополнительные поля пользователя при регистрации

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :remember_last_path
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
   def after_sign_in_path_for(user)
-    user.admin ? admin_tests_path : root_path
+    user.admin? ? admin_tests_path : root_path
   end
 
   private
