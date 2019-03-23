95f9c08914957a19712a6206e2d88fb8a328ad29
Roman <zworkbox@yandex.ru>
2019-03-23 23:26:23 +0300

    Переход на Postgresql.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ categories = Category.create!(
 
 users = User.create!(
   [
-    { first_name: "Admin", last_name: "Admin", email: "admin@mamail.ru", password: "123456", password_confirmation: "123456" },
+    { first_name: "Admin", last_name: "Admin", email: "admin@mamail.ru", password: "123456", password_confirmation: "123456", type: "Admin" },
   ]
 )
 
