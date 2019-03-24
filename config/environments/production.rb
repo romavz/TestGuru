e482647a616cb8ab3ca7a4b218e8ddef837d7b13
Roman <zworkbox@yandex.ru>
2019-03-24 16:07:09 +0300

    Исправлен номер порта в настройках SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   mailer.delivery_method = :smtp
   mailer.smtp_settings = {
     addresses:            'smtp.gmail.com',
-    port:                 577,
+    port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
